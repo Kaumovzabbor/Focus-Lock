09985fae72958d691e25753561615d5caf808895
Kaumovzabbor <kaumovzabbor@gmail.com>
2026-09-25 17:16:38 +0500

    .

--- AppLock/app/src/main/java/com/diyorbek/applock/data/InstalledAppInfo.kt
+++
@@ -1,9 +0,0 @@
-package com.diyorbek.applock.data
-
-import android.graphics.drawable.Drawable
-
-data class InstalledAppInfo(
-    val packageName: String,
-    val label: String,
-    val icon: Drawable?
-)
